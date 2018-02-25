178ba493dcc34d8f141f3cb3cd44d8aea01250f6
Michael Borromeo <mborromeo@gmail.com>
2018-02-25 16:54:43 -0500

    Add tests for the app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,68 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import {StoreModule} from '@ngrx/store';
-import {counterReducer} from './store/counter';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {Store, StoreModule} from '@ngrx/store';
+import {gridReducer} from './store/grid';
+import {AppState} from './app.module';
+import {AddDisc, ResetGame} from './store/grid.types';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let store: Store<AppState>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
       imports: [
-        StoreModule.forRoot({count: counterReducer})
+        StoreModule.forRoot({grid: gridReducer})
       ],
     }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  }));
+
+  it('should create the app component and start with player 1', async(() => {
+    expect(component).toBeTruthy();
+    expect(fixture.debugElement.query(By.css('h1')).nativeElement.textContent).toContain('Player 1\'s Turn');
+  }));
+
+  it('should call addDisc() when a column is clicked', async(() => {
+    fixture.debugElement.query(By.css('#column-1-row-0')).triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(new AddDisc(1));
+      // also check if the class for the cell has been updated
+    });
+  }));
+
+  it('should call resetGame() when a the reset button is clicked', async(() => {
+    fixture.debugElement.query(By.css('.reset-button')).triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(new ResetGame());
+    });
+  }));
+
+  it('should show a winner when the game is over', async(() => {
+    fixture.debugElement.query(By.css('#column-1-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-1-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-2-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-2-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-3-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-3-row-0')).triggerEventHandler('click', null);
+    fixture.debugElement.query(By.css('#column-4-row-0')).triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css('h1')).nativeElement.textContent).toContain('Player 1 has won');
+    });
   }));
 });
